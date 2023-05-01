9668b9d1a2131d145782e020ee9ddf3dc8ae5c11
E-Vister <blitz.vister@gmail.com>
2023-05-01 18:01:44 +0300

    Add prices payload

--- src/components/home-page-sections/price.tsx
+++ src/components/home-page-sections/price.tsx
@@ -1,37 +1,26 @@
-const PriceCol = ({title}) => {
-    //add payload for tag
-
+const PriceCol = ({label, prices}) => {
     return (
         <div className={`col-sm col-md col-lg-4 col-xl`}>
             <div className={`single-price m-auto mb-5`}>
                 <div className={`price-title text-center text-uppercase `}>
-                    <h3>{title}</h3>
+                    <h3>{label}</h3>
                 </div>
-                <div className={`tag`}>
-                    <h4 className={`tag-header`}>1 ч.</h4>
-                    <h4 className={`tag-price`}>3 р</h4>
-                </div>
-                <div className={`tag dark`}>
-                    <h4 className={`tag-header`}>3 ч.</h4>
-                    <h4 className={`tag-price`}>6 р</h4>
-                </div>
-                <div className={`tag`}>
-                    <h4 className={`tag-header`}>5 ч.</h4>
-                    <h4 className={`tag-price`}>8 р</h4>
-                </div>
-                <div className={`tag dark`}>
-                    <h4 className={`tag-header`}>День</h4>
-                    <h4 className={`tag-price`}>10 р</h4>
-                </div>
-                <div className={`tag`}>
-                    <h4 className={`tag-header`}>Ночь</h4>
-                    <h4 className={`tag-price`}>13 р</h4>
-                </div>
+                {
+                    prices.map((price, index) => (
+                            <div
+                                className={`tag ${index % 2 !== 0 ? 'dark' : ''}`}
+                                 key={`${price.amount}${price.value}`}
+                            >
+                                <h4 className={`tag-header`}>{price.amount}</h4>
+                                <h4 className={`tag-price`}>{`${price.value} р`}</h4>
+                            </div>
+                        )
+                    )
+                }
             </div>
         </div>
     )
 }
-
 
 
 export default PriceCol
--- src/components/home-page-sections/prices.tsx
+++ src/components/home-page-sections/prices.tsx
@@ -1,6 +1,58 @@
 import PriceCol from "@/components/home-page-sections/price";
 
 const Prices = () => {
+    const packages = [
+        {
+            label: `Comfort`,
+            prices: [
+                {amount: `1 ч.`, value: 3},
+                {amount: `3 ч.`, value: 6},
+                {amount: `5 ч.`, value: 8},
+                {amount: `День`, value: 10},
+                {amount: `Ночь`, value: 13}
+            ]
+        },
+        {
+            label: `VIP`,
+            prices: [
+                {amount: `1 ч.`, value: 4},
+                {amount: `3 ч.`, value: 8},
+                {amount: `5 ч.`, value: 10},
+                {amount: `День`, value: 12},
+                {amount: `Ночь`, value: 15},
+            ]
+        },
+        {
+            label: `Bootcamp`,
+            prices: [
+                {amount: `1 ч.`, value: 6},
+                {amount: `3 ч.`, value: 10},
+                {amount: `5 ч.`, value: 12},
+                {amount: `День`, value: 15},
+                {amount: `Ночь`, value: 17},
+            ]
+        },
+        {
+            label: `PS4`,
+            prices: [
+                {amount: `1 ч.`, value: 5},
+                {amount: `3 ч.`, value: 12},
+                {amount: `5 ч.`, value: 15},
+                {amount: `Ночь`, value: 18},
+            ]
+        },
+        {
+            label: `PS5`,
+            prices: [
+                {amount: `1 ч.`, value: 6},
+                {amount: `3 ч.`, value: 15},
+                {amount: `5 ч.`, value: 20},
+                {amount: `Ночь`, value: 25},
+            ]
+        },
+    ]
+
+
     return (
         <section id={`prices`} className={`prices-section`}>
             <div className={`container text-center`}>
@@ -10,11 +62,14 @@ const Prices = () => {
                     </div>
                 </div>
                 <div className={`row align-items-center justify-content-center`}>
-                    <PriceCol title={`Comfort`}/>
-                    <PriceCol title={`VIP`}/>
-                    <PriceCol title={`Bootcamp`}/>
-                    <PriceCol title={`PS4`}/>
-                    <PriceCol title={`PS5`}/>
+                    {
+                        packages.map(pack => (
+                            <PriceCol
+                                key={pack.label}
+                                label={pack.label}
+                                prices={pack.prices}/>
+                        ))
+                    }
                 </div>
             </div>
         </section>
